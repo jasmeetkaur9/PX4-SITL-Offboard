d07d513a1f0a425ff2c5363950dd988494487876
jasmeetkaur9 <jasmeet7642@gmail.com>
2022-03-07 11:49:49 +0100

    takeoff successful without QGroundControl

--- launch-common.sh
+++ launch-common.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Set PARAM_DEFINE_INT32(NAV_RCL_ACT, 1) in PX4-Autopilot
 px4_dir=/home/kaur/PX4-Autopilot
 
 source $px4_dir/Tools/setup_gazebo.bash $px4_dir $px4_dir/build/px4_sitl_default
